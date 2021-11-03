96cc244b1c5a7d554feb2f331a9f2bae4eb52ff1
shri1518 <66212194+shri1518@users.noreply.github.com>
2021-11-03 14:48:53 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,30 @@
+import React from "react"
+import "./form.css"
+import {Form, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+const LoginForm =()=>{
+  return(
+<Form>
+  <h3>Login Form</h3>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control className='inp' type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control className='inp' type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+  )}
+
+  export default LoginForm;
